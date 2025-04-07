001fce75b8f5539da84b8ee90cab88e49a6ebe23
Quynh Vo <quynh.vo3314@gmail.com>
2025-04-07 16:53:45 -0400

    comments

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -13,6 +13,13 @@ export default function Home() {
   return <>
   {/* <Welcome /> */}
   <h1>Testing</h1>
-  <BadgeReveal progress={100} />
+  <BadgeReveal progress={60} />
   </>;
 }
+
+// progress >= 16.66,
+// progress >= 33.33,
+// progress >= 50,
+// progress >= 66.66,
+// progress >= 83.33,
+// progress >= 100,
